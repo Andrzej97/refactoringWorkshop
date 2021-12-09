d10e59d11a60aa747f9f3441429a2afe74608ad8
Wojciech Witczak <70755745+wojtkolos@users.noreply.github.com>
2021-12-09 19:00:53 +0100

    Add files via upload

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -21,6 +21,7 @@ Controller::Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePo
       m_foodPort(p_foodPort),
       m_scorePort(p_scorePort)
 {
+    pause = false;
     std::istringstream istr(p_config);
     char w, f, s, d;
 
@@ -213,14 +214,31 @@ Controller::Segment Controller::getNewHead() const
     return newHead;
 }
 
+void Controller::handlePause(const PauseInd&){
+    pause = !pause;
+}
+
 void Controller::receive(std::unique_ptr<Event> e)
 {
     switch(e->getMessageId())
     {
-        case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
-        case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+        case TimeoutInd::MESSAGE_ID: {
+                if(!pause){
+                     return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e)); 
+                } else{ 
+                    break;
+                }
+            };
+        case DirectionInd::MESSAGE_ID: {
+            if(!pause){
+                return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+            } else {
+                break;
+            }
+        }
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
         default: throw UnexpectedEventException();
     };
 }
--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -37,7 +37,7 @@ private:
     void handleDirectionChange(const DirectionInd&);
     void handleFoodPositionChange(const FoodInd& receivedFood);
     void handleNewFood(const FoodResp& requestedFood);
-
+    void handlePause(const PauseInd&);
     struct Segment
     {
         int x;
@@ -60,7 +60,7 @@ private:
     IPort& m_displayPort;
     IPort& m_foodPort;
     IPort& m_scorePort;
-
+    bool pause;
     std::pair<int, int> m_mapDimension;
     std::pair<int, int> m_foodPosition;
 
